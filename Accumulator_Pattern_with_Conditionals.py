d616f0b7c519f2350e1b9d00989dea36eca94155
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-09-19 14:35:45 +0300

    Add files via upload

---
+++ Accumulator_Pattern_with_Conditionals.py
@@ -0,0 +1,26 @@
+#For each string in the list words, find the number of characters in the string. 
+#If the number of characters in the string is greater than 3, add 1 to the variable num_words
+#so that num_words should end up with the total number of words with more than 3 characters.
+
+words = ["water", "chair", "pen", "basket", "hi", "car"]
+
+num_words = 0
+for word in words:
+    if len(word) > 3:
+        num_words += 1
+        
+
+
+#Challenge: For each word in words, add ‘d’ to the end of the word if the word ends in “e” to make it past tense. 
+#Otherwise, add ‘ed’ to make it past tense. Save these past tense words to a list called past_tense.
+
+words = ["adopt", "bake", "beam", "confide", "grill", "plant", "time", "wave", "wish"]
+
+past_tense = []
+for word in words:
+    if word.endswith("e"):
+        word = word + "d"
+    else:
+        word = word + "ed"
+    past_tense.append(word)
+       
