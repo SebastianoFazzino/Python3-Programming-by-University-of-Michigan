f2cdb806729c9759f06cf441db172177ea732766
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-09-20 07:37:18 +0300

    Add files via upload

---
+++ Basics/Accumulator_Pattern_with_Lists_and_Strings.py
@@ -0,0 +1,26 @@
+nums = [3,5,8]
+accum =[]
+for i in nums:
+    x = i**2
+    accum.append(x)
+    
+print(accum)
+
+
+
+
+alist = [4,2,8,6,5]
+blist = [ ]
+for item in alist:
+   blist.append(item+5)
+print(blist)
+
+
+
+
+
+str1 = "I love python"
+char = []
+for chars in str1:
+    char.append(chars)
+print(char)
