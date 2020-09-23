1299d65bab6c841b9b8cadcf28c28686722447e8
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-09-23 20:08:22 +0300

    Update course_2_assessment_3.py

--- Python Functions, Files and Dictionaries/course_2_assessment_3.py
+++ Python Functions, Files and Dictionaries/course_2_assessment_3.py
@@ -126,3 +126,20 @@ for letter in letters:
     if letter not in letter_counts:
         letter_counts[letter] = 0
     letter_counts[letter] += 1
+    
+    
+    
+    
+
+#Create a dictionary called low_d that keeps track of all the characters in the string p and notes how many times each character was seen. 
+#Make sure that there are no repeats of characters as keys, such that “T” and “t” are both seen as a “t” for example.
+
+p = "Summer is a great time to go outside. You have to be careful of the sun though because of the heat."
+
+p_lower = p.lower()
+low_d = {}
+
+for letter in p_lower:
+    if letter not in low_d:
+        low_d[letter] = 0
+    low_d[letter] += 1
