98b02622a2761e94b5c03b71bae79b7028581e7c
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-09-28 06:48:09 +0300

    Add files via upload

--- Python Functions, Files and Dictionaries/Returning_a_Value_from_a_Function.py
+++ Python Functions, Files and Dictionaries/Returning_a_Value_from_a_Function.py
@@ -44,3 +44,73 @@ list2 = ['Natalie','Mark','Paris']
 
 print(name_length(list1))
 print(name_length(list2))
+
+
+
+
+
+def total(lst):
+    tot = 0
+    for num in lst:
+        tot = tot + num
+    return tot
+    
+    
+y = total([1,2,3])
+print(y)
+
+
+
+
+#functions can call other functions, this process is called "Composition"
+    
+def square(x):
+    y = x * x
+    return y
+
+def sum_of_square(x,y,z):
+    a = square(x)
+    b = square(y)
+    c = square(z)
+    
+    return a + b + c
+
+a = -5
+b = 2
+c = 10
+
+result = sum_of_square(a,b,c)
+print(result)
+
+
+
+
+
+#this function will return the most common letter out of a string:
+
+def most_common_letter(s):
+    frequencies = count_freq(s)
+    return best_key(frequencies)
+
+
+def count_freq(Str):
+    Dict = {}
+    for char in Str:
+        if char not in Dict:
+            Dict[char] = 0
+        Dict[char] = Dict[char] + 1
+    return Dict
+        
+        
+def best_key(dictionary):
+    Keys = dictionary.keys()
+    best_key_so_far = list(Keys)[0]
+    for key in Keys:
+        if dictionary[key] > dictionary[best_key_so_far]:
+            best_key_so_far = key
+            
+    return best_key_so_far
+
+
+sentence = input("Enter a sentence: ")
+print(most_common_letter(sentence))
