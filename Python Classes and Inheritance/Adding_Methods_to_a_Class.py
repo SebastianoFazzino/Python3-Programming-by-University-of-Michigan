daef92be891a75e303f6526cc0a32734ec3ec40b
Sebastiano Fazzino <68928594+SebastianoFazzino@users.noreply.github.com>
2020-10-07 18:38:03 +0300

    Update Adding_Methods_to_a_Class.py

--- Python Classes and Inheritance/Adding_Methods_to_a_Class.py
+++ Python Classes and Inheritance/Adding_Methods_to_a_Class.py
@@ -21,7 +21,7 @@ point1 = Point(2, 4)
 print(point1.distanceFromOrigin())
 
 
-
+#Exercise:
 #Create a class called Animal that accepts two numbers as inputs and assigns them respectively to two instance variables: arms and legs. 
 #Create an instance method called limbs that, when called, returns the total number of limbs the animal has. To the variable name spider, assign an instance of Animal that has 4 arms and 4 legs. 
 #Call the limbs method on the spider instance and save the result to the variable name spidlimbs.
